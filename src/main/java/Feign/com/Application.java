fde27fe96a1a402a101fbbe22e7ad7979f16c264
plitche <kysu728@naver.com>
2022-10-13 23:51:56 +0900

    3. Feign 사용을 위한 project setting

--- src/main/java/Feign/com/Application.java
+++ src/main/java/Feign/com/Application.java
@@ -2,8 +2,10 @@ package Feign.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // Feign Client를 사용할 것임을 알려줍니다.
 public class Application {
 
 	public static void main(String[] args) {
